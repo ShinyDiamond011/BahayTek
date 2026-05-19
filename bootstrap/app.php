4e30a952d8a8532ec94aa735e68f7053fe19dd46
ShinyDiamond011 <diongabtabz01@gmail.com>
2026-05-19 09:37:28 +0800

    fix cart

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -11,6 +11,7 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
+        $middleware->trustProxies(at: '*');
         $middleware->alias([
             'auth.user'  => \App\Http\Middleware\AuthenticateUser::class,
             'auth.staff' => \App\Http\Middleware\AuthenticateStaff::class,
